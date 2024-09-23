3a2177911a70e10de2e22bf65728ca7cf6df8078
Azzam Saefudin Rosyidi <126538984+azzamsaefudin25@users.noreply.github.com>
2024-09-23 11:28:38 +0700

    Add files via upload

---
+++ 24060122130076/Praktikum 04/PRAKTIKUM 4/admin.php
@@ -0,0 +1,45 @@
+<?php 
+// Nama         : Azzam Saefudin Rosyidi
+// NIM          : 24060122130076
+// Tanggal      : 23 September 2024
+// Nama File    : admin.php
+// Deskripsi    : untuk session halaman admin
+
+
+/*Saat kita panggil file admin.php tanpa melakukan login terlebih dahulu,
+file admin.php tidak dapat ditampilkan.
+Hal ini dikarenakan saat kita memanggil file admin.php,
+kita akan lakukan pengecekan terhadap session 'username'.
+Yang mana session tersebut baru akan diset setelah kita berhasil login.
+Sehingga jika tidak melakukan login terlebih dahulu,
+session 'username' akan tidak ter-set sehingga kita akan ter-redirect
+ke halaman login.php berdasarkan kondisional yang dibuat.
+
+Seteleah diklik logout pada file admin.php, lalu panggil kembali file admin.php,
+file admin.php tidak dapat ditampilkan.
+Hal ini karena saat kita klik logout, kita akan meng-unset session 'username' lalu
+menghancurkan session-nya. Sehingga ketika kita memanggil kembali file admin.php,
+maka kita akan ter-redirect ke halaman login.php.*/
+
+// TODO 1: Inisialisasi session
+session_start();
+
+// TODO 2: Periksa apakah session dengan key username terdefinisi
+if(!isset($_SESSION['email'])){
+    header('Location: login.php');
+    exit;
+}
+
+include('./header.php');
+?>
+<br>
+<div class="card">
+    <div class="card-header">Admin Page</div>
+    <div class="card-body">
+        <p>Welcome...</p>
+        <p>You are logged in as <b><?= $_SESSION['email']; ?></b></p>
+        <br><br>
+        <a class="btn btn-primary" href="logout.php">Logout</a>
+    </div>
+</div>
+<?php include('./footer.php') ?>
