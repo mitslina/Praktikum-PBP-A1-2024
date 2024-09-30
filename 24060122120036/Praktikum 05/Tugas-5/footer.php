9e976ff4f867c2b23cf7e9cc9e99207b70a54465
Aniqah Nursabrina <89429418+irishgurlz@users.noreply.github.com>
2024-10-01 01:48:06 +0700

    Add files via upload

---
+++ 24060122120036/Praktikum 05/Tugas-5/footer.php
@@ -0,0 +1,18 @@
+<!--
+Nama         : Aniqah Nursabrina
+NIM          : 24060122120036
+Tanggal      : 30/09/2024
+File         : footer.php
+Deskripsi    : untuk template footer
+-->
+
+</div>
+
+<!-- Bootstrap JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+<!-- TODO 1: tambahkan skrip untuk file ajax.js -->
+<script src="./ajax.js"></script>
+
+</body>
+
+</html>
