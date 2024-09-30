899d05bd716f3221ccb266c400df4ad8469a7217
Arsyad Grant S <160454738+Syadgs@users.noreply.github.com>
2024-09-30 23:52:35 +0700

    Update add_customer.php

--- 24060122140143/Praktikum 05/add_customer.php
+++ 24060122140143/Praktikum 05/add_customer.php
@@ -54,42 +54,42 @@ if (isset($_GET['submit'])) {
 <?php include('./header.php') ?>
 
 <div class="row w-50 mx-auto">
-    <div class="col">
-        <div class="card mt-4">
-            <div class="card-header">Add Customer Data</div>
-            <div class="card-body">
-                <form method="GET" autocomplete="on">
-                    <div class="mb-3">
-                        <label for="name" class="form-label">Nama:</label>
-                        <input type="text" class="form-control" id="name" name="name" value="<?php if (isset($name)) echo $name ?>">
-                        <div class="text-danger small">
-                            <p><?php if (isset($name_error)) echo $name_error ?></p>
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="address" class="form-label">Address:</label>
-                        <textarea class="form-control" id="address" rows="3" name="address"><?php if (isset($address)) echo $address ?></textarea>
-                        <div class="text-danger small">
-                            <p><?php if (isset($address_error)) echo $address_error ?></p>
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="city">City:</label>
-                        <select name="city" id="city" class="form-control" required>
-                            <option selected disabled value="none" <?php if (!isset($city)) echo 'selected' ?>>--Select a city--</option>
-                            <option value="Airport West" <?php if (isset($city) && $city == "Airport West") echo 'selected' ?>>Airport West</option>
-                            <option value="Box Hill" <?php if (isset($city) && $city == "Box Hill") echo 'selected' ?>>Box Hill</option>
-                            <option value="Yarraville" <?php if (isset($city) && $city == "Yarraville") echo 'selected' ?>>Yarraville</option>
-                        </select>
-                        <div class="text-danger small"><?php if (isset($city_error)) echo $city_error ?></div>
-                    </div>
+<div class="col">
+<div class="card mt-4">
+<div class="card-header">Add Customer Data</div>
+<div class="card-body">
+    <form method="GET" autocomplete="on">
+<div class="mb-3">
+    <label for="name" class="form-label">Nama:</label>
+    <input type="text" class="form-control" id="name" name="name" value="<?php if (isset($name)) echo $name ?>">
+ <div class="text-danger small">
+      <p><?php if (isset($name_error)) echo $name_error ?></p>
+ </div>
+</div>
+ <div class="mb-3">
+      <label for="address" class="form-label">Address:</label>
+           <textarea class="form-control" id="address" rows="3" name="address"><?php if (isset($address)) echo $address ?></textarea>
+               <div class="text-danger small">
+      <p><?php if (isset($address_error)) echo $address_error ?></p>
+       </div>
+          </div>
+            <div class="mb-3">
+               <label for="city">City:</label>
+               <select name="city" id="city" class="form-control" required>
+             <option selected disabled value="none" <?php if (!isset($city)) echo 'selected' ?>>--Select a city--</option>
+                <option value="Airport West" <?php if (isset($city) && $city == "Airport West") echo 'selected' ?>>Airport West</option>
+          <option value="Box Hill" <?php if (isset($city) && $city == "Box Hill") echo 'selected' ?>>Box Hill</option>
+               <option value="Yarraville" <?php if (isset($city) && $city == "Yarraville") echo 'selected' ?>>Yarraville</option>
+               </select>
+           <div class="text-danger small"><?php if (isset($city_error)) echo $city_error ?></div>
+         </div>
 
-                    <button type="button" class="btn btn-primary" onclick="add_customer_get()">Add Customer (GET)</button>
-                </form>
-                <br>
+    <button type="button" class="btn btn-primary" onclick="add_customer_get()">Add Customer (GET)</button>
+    </form>
+       <br>
                 <div id="add_response"></div>
-            </div>
-        </div>
+         </div>
+   </div>
     </div>
 </div>
 
