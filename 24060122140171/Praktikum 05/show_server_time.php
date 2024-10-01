0f80c490df252836ea567d1cf98bbdc7ff549e1e
FikriAzka <142158263+FikriAzka@users.noreply.github.com>
2024-10-01 09:36:58 +0700

    Add files via upload

---
+++ 24060122140171/Praktikum 05/show_server_time.php
@@ -0,0 +1,22 @@
+<!-- 
+Nama         : Fikri Azka Pradya
+NIM          : 24060122140171
+Tanggal      : 1 Oktober 2024
+File         : show_server_time.php
+Deskripsi    : Menampilkan waktu server dengan ajax
+-->
+
+<?php require_once("header.php"); ?> <br>
+<div class="card">
+    <div class="card-header">Ajax Server Time</div>
+    <div class="card-body">
+        <!-- TODO: Membuat tombol Show Server Time -->
+        <button class="btn btn-success" onclick="get_server_time()">Show Server Time</button>
+        <br><br>
+        <!-- TODO: Membuat elemen untuk menampilkan server time -->
+        <div id="showtime"></div>
+    </div>
+</div>
+
+<?php require_once("footer.php"); ?>
+
