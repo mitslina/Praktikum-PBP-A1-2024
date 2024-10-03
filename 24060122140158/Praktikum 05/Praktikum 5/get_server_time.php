9d74985cd26902f7385673e05e494f698bebbad2
WSevens7 <114938184+WSevens7@users.noreply.github.com>
2024-10-03 10:33:51 +0700

    Add files via upload

---
+++ 24060122140158/Praktikum 05/Praktikum 5/get_server_time.php
@@ -0,0 +1,9 @@
+<!-- 
+Nama         : Adib Willy Kusuma Adrigantara
+NIM          : 24060122140158
+Tanggal      : 30 September 2024
+ -->
+<?php 
+// TODO 1: Tuliskan response berupa waktu server saat ini
+    echo date('H:i:s');
+?>
