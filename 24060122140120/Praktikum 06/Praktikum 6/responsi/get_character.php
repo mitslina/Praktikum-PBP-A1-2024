2086f62f61a14760b5fdbb283821623c37484d27
bismawira20 <117698894+bismawira20@users.noreply.github.com>
2024-12-07 11:52:47 +0700

    Add files via upload

---
+++ 24060122140120/Praktikum 06/Praktikum 6/responsi/get_character.php
@@ -0,0 +1,19 @@
+<?php
+//Nama :Bisma Wira Adi Wicaksono
+//NIM : 24060122140120
+//Kelas : A1
+
+require_once 'lib/db_login.php';
+
+if (isset($_POST['email'])) {
+    $email = $_POST['email'];
+    $query = "SELECT * FROM tb_characters WHERE email='$email'";
+    $result = $db->query($query);
+
+    if ($result->num_rows > 0) {
+        echo "taken";
+    } else {
+        echo "available";
+    }
+}
+?>
