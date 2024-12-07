8009e8f351cb6527090ff5fddb0bc24bfc80ea1e
bismawira20 <117698894+bismawira20@users.noreply.github.com>
2024-12-07 12:02:47 +0700

    Add files via upload

---
+++ 24060122140120/Praktikum 06/get_character.php
@@ -0,0 +1,31 @@
+<!-- 
+Nama         : Bisma Wira Adi Wicaksono
+NIM          : 24060122140120
+Tanggal      : 1 Oktober 2024
+File         : get_character.php
+ -->
+<?php
+require_once 'lib/db_login.php';
+
+// // TODO 4 : MENGAMBIL DATA USER DARI TABEL 'tb_characters' DENGAN PARAMETER EMAIL
+
+if (isset($_POST['email'])) {
+    $email = test_input($_POST['email']);
+
+    // Jika format email tidak valid
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Email format is incorrect";
+        exit;
+    }
+    // Query untuk memeriksa email di database
+    $query = "SELECT * FROM tb_characters WHERE email = '$email'";
+    $result = $db->query($query);
+
+    if ($result->num_rows > 0) {
+        echo "email has been used";  // Jika email sudah digunakan
+    } else {
+        echo "email is available";  // Jika email tersedia
+    }
+}
+
+?>
---
+++ 24060122140120/Praktikum 06/readme.md
@@ -0,0 +1,1 @@
+Pastikan Nama dan NIM Anda Benar!!! NAMA: "BISMA WIRA ADI WICAKSONO" NIM: 24060122140120 
