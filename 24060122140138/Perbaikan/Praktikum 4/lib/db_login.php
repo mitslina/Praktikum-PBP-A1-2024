c70a0c6ac67b1d11af10f1e0d94c3c29747a0182
adzkiyaqarina <108077074+adzkiyaqarina@users.noreply.github.com>
2024-12-07 14:06:43 +0700

    Add files via upload

---
+++ 24060122140138/Perbaikan/Praktikum 4/lib/db_login.php
@@ -0,0 +1,24 @@
+<?php 
+// Nama      : Mochammad Qaynan Mahdaviqya
+// NIM       : 24060122140170
+// Deskripsi : Untuk konekksi ke database
+
+// TODO 1: Buatlah koneksi dengan database
+$db_host = 'localhost';
+$db_database = 'bookorama';
+$db_username = 'root';
+$db_password = '';
+
+$db = new mysqli($db_host, $db_username, $db_password, $db_database);
+if($db->connect_errno){
+    die('Could not connect to database: <br />'. $db->connect_error);
+}
+
+// TODO 2: Buatlah function test_input
+function test_input($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
